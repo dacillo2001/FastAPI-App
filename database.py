9faafde59af85501c0b06b0476c23891a5540c6b
dacillo2001 <dacillonicole8@gmail.com>
2025-04-15 20:03:24 +0800

    commit

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Using environment variable for database URL
-database_url = os.environ.get("postgresql://dbname_re6j_user:HlBY5FpYoVjXlBUNS4bQBpLMzEqsZ1zj@dpg-cvv3q6adbo4c73fdt44g-a/dbname_re6j")
+database_url = os.environ.get("postgresql://dbname_re6j_user:HlBY5FpYoVjXlBUNS4bQBpLMzEqsZ1zj@dpg-cvv3q6adbo4c73fdt44g-a:5432/dbname_re6j")
 # Creating the engine to interact with PostgreSQL
 engine = create_engine(database_url, echo=True)
 # Creating a session local class to interact with the database
