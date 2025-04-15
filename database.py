e0a2ce386cfda3561d5648cc2e06b1bbad5d9096
dacillo2001 <dacillonicole8@gmail.com>
2025-04-15 20:33:03 +0800

    commit

--- database.py
+++ database.py
@@ -1,12 +1,20 @@
-import os
+# database.py
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-# Using environment variable for database URL
-database_url = os.environ.get("postgresql://dbname_re6j_user:HlBY5FpYoVjXlBUNS4bQBpLMzEqsZ1zj@dpg-cvv3q6adbo4c73fdt44g-a:5432/dbname_re6j")
-# Creating the engine to interact with PostgreSQL
-engine = create_engine(database_url, echo=True)
-# Creating a session local class to interact with the database
+load_dotenv()
+
+# Load the DATABASE_URL from environment variables
+DATABASE_URL = os.getenv("postgresql://dbname_re6j_user:HlBY5FpYoVjXlBUNS4bQBpLMzEqsZ1zj@dpg-cvv3q6adbo4c73fdt44g-a:5432/dbname_re6j")
+
+# Set up the database connection
+engine = create_engine(DATABASE_URL)
+
+# Set up the session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base class to create tables
+
+# Base class for our models
 Base = declarative_base()
